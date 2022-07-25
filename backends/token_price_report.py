50baf80ab060da369875534a014b8ed7871773c9
sjl_glb <goodMorning_glb@test.com>
2022-07-25 22:06:12 +0800

    add token price report

---
+++ backends/token_price_report.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append('../')
+
+import sys
+from db_provider import summary_hourly_price, price_report
+
+
+def handle_token_price_report(network_id):
+    try:
+        summary_hourly_price()
+        price_report(network_id)
+    except Exception as e:
+        print("Error occurred when clear db data, Error is: ", e)
+
+
+if __name__ == '__main__':
+    # update_price("TESTNET")
+    if len(sys.argv) == 2:
+        network_id = str(sys.argv[1]).upper()
+        if network_id in ["MAINNET", "TESTNET", "DEVNET"]:
+            handle_token_price_report()
+        else:
+            print("Error, network_id should be MAINNET, TESTNET or DEVNET")
+            exit(1)
+    else:
+        print("Error, must put NETWORK_ID as arg")
+        exit(1)
