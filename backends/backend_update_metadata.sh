794a25ad090ab452dc76d746181cfb2c5fa1537e
dom-jiang <805816775@.qq.com>
2024-04-24 19:16:44 +0800

    add job

---
+++ backends/backend_update_metadata.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+pid=`ps -ef | grep "update_metadata.py MAINNET" | grep -v grep | /usr/bin/awk '{print $2}'`
+
+cd "/indexer-helper/backends"
+
+# echo ${pid}
+date >> backend_update_metadata.log
+
+if [ ! ${pid} ]; then
+        # echo "is null"
+        echo "No backend process rubbish to clean." >> backend_update_metadata.log
+else
+        # echo "not null"
+        kill -s 9 ${pid}
+        echo "Warning: clean backend process of last round." >> backend_update_metadata.log
+fi
+. ../venv/bin/activate
+python update_metadata.py MAINNET >> backend_update_metadata.log
+echo 'OK'
