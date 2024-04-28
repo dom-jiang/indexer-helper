0cd9813c94d9a3f7a027e2c96ad8d51b36cb01ff
dom-jiang <805816775@.qq.com>
2024-04-28 17:14:23 +0800

    update job

--- backends/analysis_pool_and_farm_data_s3.py
+++ backends/analysis_pool_and_farm_data_s3.py
@@ -3,10 +3,10 @@ import os
 import json
 import shutil
 import sys
-from db_info import BUCKET_NAME, AWS_S3_AKI, AWS_S3_SAK
 import time
 
 sys.path.append('../')
+from db_info import BUCKET_NAME, AWS_S3_AKI, AWS_S3_SAK
 from db_provider import add_account_assets_data, get_token_price, handle_account_pool_assets_data
 import decimal
 from config import Cfg
