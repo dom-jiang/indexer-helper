78592ebe6669c7e3453dc6f707329cf69da424ea
dom-jiang <805816775@.qq.com>
2024-10-25 15:23:38 +0800

    Update rpc

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ try:
     from rpc_info import TESTNET_RPC_URL, MAINNET_RPC_URL
 except ImportError:
     TESTNET_RPC_URL= ["https://rpc.testnet.near.org", ]
-    MAINNET_RPC_URL= ["http://45.77.96.53:3030", "https://rpc.mainnet.near.org"]
+    MAINNET_RPC_URL= ["https://rpc.mainnet.near.org", "https://near.drpc.org", "https://free.rpc.fastnear.com", "https://rpc.mainnet.pagoda.co", "http://45.77.96.53:3030"]
 
 try:
     from db_info import REDIS_HOST, REDIS_PORT
