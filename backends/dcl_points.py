629ea5cbebef477448c08ad858e19559017b4dca
dom-jiang <805816775@.qq.com>
2025-10-30 20:09:58 +0800

    Add dcl points task

--- backends/dcl_points.py
+++ backends/dcl_points.py
@@ -6,7 +6,7 @@ sys.path.append('../')
 
 def handel_dcl_points():
     dcl_pool_list = []
-    request_url = "https://172.17.0.1:8081/pool/search?type=dcl&sort=24h&limit=30&labels=&offset=0&hide_low_pool=false&hide_uncertified_token=false&order_by=desc&token_type=&token_list=&pool_id_list="
+    request_url = "http://172.17.0.1:8081/pool/search?type=dcl&sort=24h&limit=30&labels=&offset=0&hide_low_pool=false&hide_uncertified_token=false&order_by=desc&token_type=&token_list=&pool_id_list="
     dcl_pool_list_ret = requests.get(request_url).text
     dcl_pool_data_ret = json.loads(dcl_pool_list_ret)
     if dcl_pool_data_ret["code"] == 0:
@@ -15,14 +15,14 @@ def handel_dcl_points():
             if float(dcl_pool_data["volume_24h"]) > 0:
                 dcl_pool_list.append(dcl_pool_data["id"])
     for pool_id in dcl_pool_list:
-        request_url = "https://172.17.0.1:8000/get-dcl-points?pool_id=" + pool_id
+        request_url = "http://172.17.0.1:8000/get-dcl-points?pool_id=" + pool_id
         print("request_url:", request_url)
         dcl_pool_point = requests.get(request_url).text
         print("dcl_pool_point:", dcl_pool_point)
 
 
 if __name__ == '__main__':
-    print("start rhea data task")
+    print("start dcl_points task")
     if len(sys.argv) == 2:
         network_id = str(sys.argv[1]).upper()
         if network_id in ["MAINNET", "TESTNET", "DEVNET"]:
