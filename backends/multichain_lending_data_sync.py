03f6727a7daaaf18d2351513ab129c4cd548fe8d
dom-jiang <805816775@.qq.com>
2025-11-11 17:14:12 +0800

    Update multichain lending job

--- backends/multichain_lending_data_sync.py
+++ backends/multichain_lending_data_sync.py
@@ -1,9 +1,10 @@
 import json
 import sys
+import requests
 sys.path.append('../')
 
-import sys
 from db_provider import update_multichain_lending_report_data, get_multichain_lending_report_data, get_multichain_lending_requests_history
+from near_multinode_rpc_provider import MultiNodeJsonProvider
 
 
 def handle_multichain_lending_data(network_id):
@@ -18,8 +19,34 @@ def handle_multichain_lending_data(network_id):
                     if "tx_hash" in request_result:
                         tx_hash = request_result["tx_hash"]
                     update_multichain_lending_report_data(network_id, requests_history["request_result"], multichain_lending["id"], tx_hash)
+            elif multichain_lending["type"] == 2:
+                request_hash = multichain_lending["request_hash"]
+                tx_status = check_tx_status_direct(network_id, request_hash)
+                if tx_status is not None:
+                    update_multichain_lending_report_data(network_id, json.dumps(tx_status), multichain_lending["id"], request_hash)
     except Exception as e:
         print("Error handle_multichain_lending_data, Error is: ", e)
+
+
+def check_tx_status_direct(network_id, tx_hash):
+    ret_data = {"tx_hash": tx_hash, "tx_err_msg": ""}
+    conn = MultiNodeJsonProvider(network_id)
+    try:
+        ret = conn.get_tx(tx_hash, "intents.near")
+        if "status" in ret:
+            status = ret['status']
+            if 'SuccessValue' in status:
+                return ret_data
+            elif 'Failure' in status:
+                ret_data["tx_err_msg"] = json.dumps(status['Failure'])
+                return ret_data
+    except requests.exceptions.RequestException as e:
+        print(f"网络请求错误: {e}")
+    except json.JSONDecodeError as e:
+        print(f"JSON 解析错误: {e}")
+    except Exception as e:
+        print(f"未知错误: {e}")
+    return None
 
 
 if __name__ == '__main__':
@@ -34,5 +61,8 @@ if __name__ == '__main__':
     else:
         print("Error, must put NETWORK_ID as arg")
         exit(1)
+
     # handle_multichain_lending_data("MAINNET")
+    # rr = check_tx_status_direct("MAINNET", "GGb8aFcY4KepPYtPVVp4eDqLKtdXLDeVzsXWnuiVuWi4")
+    # print("1111111:", json.dumps(rr))
     print("end handle_multichain_lending_data")
