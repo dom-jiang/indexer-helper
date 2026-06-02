e1eaa6d77d9ed6122df8d84c0b9fa75d8051df65
dom-jiang <805816775@.qq.com>
2026-06-02 16:25:15 +0800

    Update Unified Swap job

--- backends/backend_swap_status_checker.sh
+++ backends/backend_swap_status_checker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pid=`ps -ef | grep "backend_swap_status_checker.py MAINNET" | grep -v grep | /usr/bin/awk '{print $2}'`
+pid=`ps -ef | grep "swap_status_checker.py MAINNET" | grep -v grep | /usr/bin/awk '{print $2}'`
 
 cd "/indexer-helper/backends"
 
@@ -12,5 +12,5 @@ else
         kill -s 9 ${pid}
         echo "Warning: killed previous backend_swap_status_checker." >> backend_swap_status_checker.log
 fi
-/usr/local/bin/python backend_swap_status_checker.py MAINNET >> backend_swap_status_checker.log
+/usr/local/bin/python swap_status_checker.py MAINNET >> backend_swap_status_checker.log
 echo 'OK'
